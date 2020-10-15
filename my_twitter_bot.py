6eaa3a40154ef1c5ddd8fd4d249d186cbced8215
renaudfournet <fournet.renaud@gmail.com>
2020-10-15 13:10:15 +0200

    First commit

---
+++ my_twitter_bot.py
@@ -0,0 +1,48 @@
+import tweepy
+from time import sleep
+from credentials import *
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+
+file = open('verne.txt', 'r')            # fichier texte
+status = file.readlines()
+file.close()
+
+def tweet_line():                           # fonction
+    for line in status:
+        try:
+            print(line)
+            sleep(1800)
+            api.update_status(line)
+            if line != '\n':               # passer les lignes blanches
+                api.update_status(line)
+            else:
+                pass
+        except tweepy.TweepError as e:
+            if e.api_code == 187:
+                print('Duplicate')
+                sleep(5)
+            elif e.api_code == 170:
+                print('Blank')
+                sleep(2)
+            else:
+                pass
+
+
+tweet_line()
+
+
+
+
+
+
+
+
+
+
+
